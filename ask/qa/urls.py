d829046477c988a0dde3e7e15163f35ffc625b54
qu0ch <qu0ch@front.ru>
2016-05-17 23:45:26 +0500

    fix urls.py

---
+++ ask/qa/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from ask.qa.views import test
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^', test, name='main'),
+    url(r'^login/', test, name='login'),
+    url(r'^signup/', test, name='signup'),
+    url(r'^question/(?P<id>[\d]+)$/', test, name='test'),
+    url(r'^ask/', test, name='ask'),
+    url(r'^popular/', test, name='popular'),
+    url(r'^new/', test, name='new'),
+)
