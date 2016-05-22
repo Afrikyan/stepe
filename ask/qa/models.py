650c5492d36d7b07f8b92b39e35b40db8f10af1b
qu0ch <qu0ch@front.ru>
2016-05-22 21:25:10 +0500

    add BD

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(
+        User, related_name="question_like", blank=True)
+
+    class Meta:
+        ordering = ('-added_at',)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ('added_at',)
+
+    def __unicode__(self):
+        return self.author
--- init.sh
+++ init.sh
@@ -5,3 +5,9 @@ sudo /etc/init.d/nginx restart
 # Gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+
+# MySQL
+echo 'innodb_use_native_aio = 0' | sudo tee --append /etc/mysql/my.cnf
+sudo service mysql restart
+sudo mysql -uroot -e "CREATE DATABASE ask CHARACTER SET utf8 COLLATE utf8_general_ci;"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'ask_user'@'localhost' IDENTIFIED BY '1qaz2wsx';"
