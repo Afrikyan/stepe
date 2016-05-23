cd4e950cf90d866439a32fe3c02d99cba95368b2
qu0ch <qu0ch@front.ru>
2016-05-23 20:10:55 +0500

    form edit

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -56,12 +56,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-# DATABASES = {
+#DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
-# }
+#}
 
 DATABASES = {
     'default': {
--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -3,11 +3,23 @@ from django.shortcuts import get_object_or_404
 from qa.models import Question, Answer
 
 
-class AskForm(forms.ModelForm):
+class AskForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    text = forms.CharField(widget=forms.Textarea)
 
-    class Meta:
-        model = Question
-        fields = ('title', 'text')
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.strip() == '':
+            raise forms.ValidationError(
+                u'Title is empty', code='validation_error')
+        return title
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if text.strip() == '':
+            raise forms.ValidationError(
+                u'Text is empty', code='validation_error')
+        return text
 
     def save(self):
         if self._user.is_anonymous():
